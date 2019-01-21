22df8490c29ff15ae574b5cf9fc4e3d7ae95ba2d
Spencer Olson <solson@instructure.com>
2019-01-20 18:02:28 -0600

    append encryption key file

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,8 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+append :linked_files, "config/secrets.yml.key"
+
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
